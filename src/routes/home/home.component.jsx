7119253377f86b3f3f55453c8a30549ee041b94e
Melody Anaka <anakamelody909@gmail.com>
2024-11-06 03:26:53 +0100

    took out categories from the home components

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 const Home = () => {
   return (
     <div>
-      <Directory categories={categories} />
+      <Directory />
       <Outlet/>
     </div>
   );
